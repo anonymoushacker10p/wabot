195aadeda566c90e112f8c43d4bf9f2abce357f9
anonymoushacker10p <106658440+anonymoushacker10p@users.noreply.github.com>
2023-02-09 21:19:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,13 +14,13 @@ global.APIs = {
 
 // Apikey Website Api
 global.APIKeys = {
-	'https://api.zahwazein.xyz': 'Your Key',
+	'https://api.zahwazein.xyz': '95f49ff20611',
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['62895348056622','6285839601005']
+global.premium = ['62895348056622']
+global.packname = 'ridhzbot'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
